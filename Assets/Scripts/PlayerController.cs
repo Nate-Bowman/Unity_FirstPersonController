c5e95c73f3a562ba8fa040e0de3cfb1a2b3c526a
Nate-Bowman <73073316+Nate-Bowman@users.noreply.github.com>
2020-10-18 19:53:34 +0100

    Add and apply gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private const float Gravity = 9.8f;
+	private CharacterController _characterController;
+	private Vector3 _characterVelocity;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	private void Start()
+	{
+		// cache the character controller component
+		_characterController = GetComponent<CharacterController>();
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		// Was i grounded last frame?
+		var wasGrounded = _characterController.isGrounded;
+
+		if (_characterController.isGrounded)
+		{
+			_characterVelocity = Vector3.zero;
+
+			//TODO: Add movement controls
+		}
+		else
+		{
+			// add gravitational acceleration this frame
+			_characterVelocity += Vector3.down * (Gravity * Time.deltaTime);
+
+			//TODO: Add air strafe
+		}
+
+		// Apply calculated movement
+		_characterController.Move(_characterVelocity * Time.deltaTime);
+	}
 }
