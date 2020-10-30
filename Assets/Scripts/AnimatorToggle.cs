337fab051d29fd95e29e6792943abf46cc5c7b4c
Nate-Bowman <73073316+Nate-Bowman@users.noreply.github.com>
2020-10-30 13:37:27 +0000

    Added animator toggle

---
+++ Assets/Scripts/AnimatorToggle.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class AnimatorToggle : InteractiveObject
+{
+	[SerializeField]
+	private Animator _Animator;
+
+	[SerializeField]
+	private string AnimationTriggerName = "Toggle";
+	
+	private void Start()
+	{
+		_Animator = GetComponent<Animator>();
+	}
+
+	/// <inheritdoc cref="InteractiveObject.DoAction" />
+	public override void DoAction(Transform caller)
+	{
+		_Animator.SetTrigger(AnimationTriggerName);
+	}
+}
